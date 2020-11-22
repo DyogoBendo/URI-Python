e31150a13f4d4844d22195717d3311a23742a25d
DyogoBendo <dyogoromagnabendo@gmail.com>
2020-11-22 11:28:13 -0300

    Área esquerda

---
+++ 1189.py
@@ -0,0 +1,22 @@
+def soma(a):
+    return sum(a)
+
+
+def media(a):
+    return soma(a) / len(a)
+
+
+if __name__ == '__main__':
+    acao = input()
+    vetor_valores_abaixo = []
+
+    for i in range(12):
+        for j in range(12):
+            numero = float(input())
+            if j <= 4:
+                if j + i < 11 and j - i < 0:
+                    vetor_valores_abaixo.append(numero)
+    if acao == 'S':
+        print(f"{soma(vetor_valores_abaixo):.1f}")
+    else:
+        print(f"{media(vetor_valores_abaixo):.1f}")
