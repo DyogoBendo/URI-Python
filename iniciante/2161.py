40a6183debe0b07d4e0467fa992591c1a06cbbdf
DyogoBendo <dyogoromagnabendo@gmail.com>
2020-12-13 12:36:25 -0300

    Raiz Quadrada de 10

---
+++ iniciante/2161.py
@@ -0,0 +1,12 @@
+def calc_den(n):
+	if n == 1:
+		return 1/6
+	else:
+		return 1/ (6 + calc_den(n-1))
+
+n = int(input())
+resposta=3.0
+if n!=0:
+	resposta+= calc_den(n)
+
+print(f'{resposta:.10f}')
