a498fe65c06df19df181ab38eb899c3494b11e0c
DyogoBendo <dyogoromagnabendo@gmail.com>
2020-12-17 13:09:31 -0300

    Notas da Prova

--- iniciante/2344.py
+++ iniciante/2344.py
@@ -1,1 +1,12 @@
-Notas da Prova
+if __name__ == '__main__':
+    entrada = int(input())
+    if entrada == 0:
+        print('E')
+    elif 1 <= entrada <= 35:
+        print('D')
+    elif 36 <= entrada <= 60:
+        print('C')
+    elif 61 <= entrada <= 85:
+        print('B')
+    elif 86 <= entrada <= 100:
+        print('A')
