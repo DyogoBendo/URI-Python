7a05cd5cc76591911b0b96c770a42baa7c31ff50
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-01-07 21:04:38 -0300

    Nova tentativa...

--- iniciante/2632.py
+++ iniciante/2632.py
@@ -1,5 +1,17 @@
-def coordenada_bate(retangulo, circulo, raio):
-    distancia = (((retangulo[0] - circulo[0]) ** 2) + ((retangulo[1] - circulo[1]) ** 2)) ** 0.5
+def calcula_distancia (v, w):
+    return (v['x'] - w['x'])**2 + (v['y'] - w['y'])**2
+
+
+def coordenada_bate(c, v, w):
+    l2 = calcula_distancia(v, w)
+    if l2 == 0:
+        return False
+    t = ((c['x'] - v['x']) * (w['x'] - v['x']) + (c['y'] - v['y']) * (w['y'] - v['y'])) / l2
+    answer = {
+        'x': v['x'] + t * (w['x'] - v['x']),
+        'y': v['y'] + t * (w['y'] - v['y'])
+    }
+    distancia = (calcula_distancia(c, answer)) ** 0.5
     if distancia <= raio:
         return True
     else:
@@ -12,6 +24,7 @@ if __name__ == '__main__':
         w, h, yo, xo = map(int, input().split())
         are_commum = False
         magia, nivel, cx, cy = input().split()
+        cx, cy = map(int, [cx, cy])
         if magia == 'fire':
             if nivel == '1':
                 raio = 20
@@ -45,14 +58,33 @@ if __name__ == '__main__':
                 raio = 60
             dano = 100
 
-        esquerda_cima = xo, yo + h
-        esquerda_baixo = xo, yo
-        direita_cima = xo + h, yo + h
-        direita_baixo = xo + h, yo
-        circulo = int(cx), int(cy)
+        circulo = {
+            'x': cx,
+            'y': cy,
+            'raio': raio
+        }
 
-        if coordenada_bate(esquerda_cima, circulo, raio) or coordenada_bate(esquerda_baixo, circulo, raio) \
-                or coordenada_bate(direita_baixo, circulo, raio) or coordenada_bate(direita_cima, circulo, raio):
+        ponto1 = {
+            'x': xo,
+            'y': yo
+        }
+
+        ponto2 = {
+            'x': xo + w,
+            'y': yo
+        }
+
+        ponto3 = {
+            'x': xo,
+            'y': yo + h
+        }
+
+        ponto4 = {
+            'x': xo + w,
+            'y': yo + h
+        }
+
+        if coordenada_bate(circulo, ponto1, ponto2) or coordenada_bate(circulo, ponto1, ponto3) or coordenada_bate(circulo, ponto2, ponto4) or coordenada_bate(circulo, ponto3, ponto4):
             print(dano)
         else:
             print(0)
