4e49da5a096fef6e5e1a40a3cb9a52ee6d80d5ae
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-21 23:10:48 -0300

    Entrada e Saída 6

---
+++ iniciante/2762.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    entrada = input()
+    parte_1, parte_2 = entrada.split(".")
+    parte_1 = int(parte_1)
+    parte_2 = int(parte_2)
+    print(f"{parte_2}.{parte_1}")
