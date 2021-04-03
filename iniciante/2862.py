b58633bb3c356c5a5c77de921c7903cdec070ea6
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-04-02 21:23:43 -0300

    Inseto!

---
+++ iniciante/2862.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    repete = int(input())
+    for i in range(repete):
+        ki = int (input())
+        if ki > 8000:
+            print("Mais de 8000!")
+        else:
+            print("Inseto!")
