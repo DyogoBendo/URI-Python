23ca885e4fd8490d26615a62b68d03f5ee4d1aea
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-18 09:40:31 -0300

    Tamanho da Placa

--- iniciante/2770.py
+++ iniciante/2770.py
@@ -1,14 +1,31 @@
+import sys
+
+def calcula_dimensao (menor0, maior0, menor, maior):     
+    if maior > maior0:
+        return False
+    if menor > menor0:
+        return False
+    return True
+
+
+def get_ints(): return map(int, sys.stdin.readline().strip().split())
+
+
 if __name__ == "__main__":
     while True:
-        try:            
-            xo, yo, m = map(int, input().split())
-            soma0 = xo + yo
+        try:               
+            xo, yo, m = get_ints()
+            menor0 = xo if xo < yo else yo
+            maior0 = xo if yo == menor0 else yo           
             for i in range(m):
-                x, y = map(int, input().split())
-                soma = x + y 
-                if soma <= soma0:
-                    print("Sim")
+                x, y = get_ints()
+                
+                menor = x if x < y else y
+                maior = x if x >= y else y
+
+                if calcula_dimensao (menor0, maior0, menor, maior):
+                    sys.stdout.write("Sim\n")
                 else:
-                    print("Nao")
-        except EOFError:
+                    sys.stdout.write("Nao\n")
+        except:
             break
