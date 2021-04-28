da56d249ee8555d4f376411b48f853c99bb73793
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-28 12:54:18 -0300

    Quanta Mandioca?

---
+++ iniciante/2936.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    a = int(input()) * 300
+    b = int(input()) * 1500
+    c = int(input()) * 600
+    d = int(input()) * 1000
+    e = int(input()) * 150
+
+    t = a + b + c + d + e + 225
+    print(t)
