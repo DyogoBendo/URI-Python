1e48bd81011c8c92eba108e1990de80948fbcd57
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-27 22:49:45 -0300

    Saída 9

---
+++ iniciante/2755.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    b = "'b'"
+    face = "('.')"
+    print(f'"Ro{b}er\tto\/"')
+    print("(._.) ( l: ) ( .-. ) ( :l ) (._.)")
+    print("(^_-) (-_-) (-_^)")
+    print(f'("_") {face}')
