87b16cbbb82a3eb00b60d6f78758da132f1dd208
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-28 12:19:57 -0300

    Piso da Escola

---
+++ iniciante/2786.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    l = int(input())
+    c = int(input())
+
+    tipo_1 = (l * c) + ( (l - 1) * (c - 1))
+    tipo_2 = (2 * (l - 1)) + (2 * (c - 1))
+
+    print(tipo_1)
+    print(tipo_2)
+
+    
