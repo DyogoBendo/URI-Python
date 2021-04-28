66a641c45dff6ba9fc03197345433d40ea199668
dyogobendo <dyogoromagnabendo@gmail.com>
2021-04-27 22:32:42 -0300

    Saída 6

---
+++ iniciante/2752.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":    
+    print("<AMO FAZER EXERCICIO NO URI>")
+    print("<    AMO FAZER EXERCICIO NO URI>")
+    print("<AMO FAZER EXERCICIO >")
+    print("<AMO FAZER EXERCICIO NO URI>")
+    print("<AMO FAZER EXERCICIO NO URI    >")
+    print("<AMO FAZER EXERCICIO NO URI>")
+    print("<          AMO FAZER EXERCICIO >")
+    print("<AMO FAZER EXERCICIO           >")
