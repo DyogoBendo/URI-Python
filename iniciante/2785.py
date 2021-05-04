556625b0ad3bf40524b4700f2ae4c8389b51f4d2
dyogobendo <dyogoromagnabendo@gmail.com>
2021-05-04 14:00:58 -0300

    Pirâmide

--- iniciante/2785.py
+++ iniciante/2785.py
@@ -1,30 +1,38 @@
 from math import inf
 
-
-def calcula_piramide(l, i, j, m):
-    if l == 0:
-        return m[l][i]
-
-    print(l, i, j)
-    v = sum(m[l][i:j])
-    v1 = calcula_piramide(l - 1, i, l, m )
-    v2 = calcula_piramide(l - 1, i + 1,  l + 1, m)
-    v3 = v1 if v1 < v2 else v2
-    v += v3                        
-    return v
-
-
 if __name__ == "__main__":
     n = int(input())
 
-    matriz = []
+    m = []
 
     for i in range(n):
         entrada = list(map(int, input().split()))
 
-        matriz.append(entrada)
-    
-    r = calcula_piramide(n - 1, 0, n - 1, matriz)
+        m.append(entrada)
 
-    print(r)
+    vm = [[0 for i in range(n)] for j in range(n)]
+
+    k = 0
+    for i in range(n):                    
+        vm[i][i] = m[0][k]
+        k += 1
+
+    l = 1  # linha    
+    p = 0
+    for l in range(1, n):
+        p += 1
+        i = 0  # coluna limite inicial        
+        for j in range(i + p, n):
+            v1 = vm[i][j - 1]
+            v2 = vm[i + 1][j]
+            sup = v1 if v1 < v2 else v2
+
+            s = sum(m[l][i:j + 1]) + sup
+            vm[i][j] = s
+
+            i += 1            
+    
+    print(vm[0][n - 1])
+
+
         
