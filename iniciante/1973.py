bde4cf8c8bb164a7bb4b80512537370b0f07698c
dyogobendo <dyogoromagnabendo@gmail.com>
2021-05-10 13:59:18 -0300

    Jornada nas Estrelas

--- iniciante/1973.py
+++ iniciante/1973.py
@@ -1,29 +1,18 @@
 if __name__ == '__main__':
     num_estrelas = int(input())
-    estrelas = list(map(int, input().split()))
-
-    fazendas_visitadas = [0] * num_estrelas
+    estrelas = list(map(int, input().split()))    
+    fazendas_visitadas = [0 for _ in range(num_estrelas)]
 
     i = 0
-    while True:
-        if i == 0 and estrelas[i] % 2 == 0:
-            fazendas_visitadas[i] = 1
-            if estrelas[i] > 0:
+    while i < num_estrelas:
+        fazendas_visitadas[i] = 1
+        is_par = estrelas[i] % 2 == 0
+        if estrelas[i] > 0:
                 estrelas[i] -= 1
-            break
-        if i == num_estrelas - 1 and estrelas[i] % 2 != 0:
-            fazendas_visitadas[i] = 0
-            if estrelas[i] > 0:
-                estrelas[i] -= 1
-            break
-        elif estrelas[i] % 2 == 0:
-            fazendas_visitadas[i] = 1
-            if estrelas[i] > 0:
-                estrelas[i] -= 1
+        if i == 0 and is_par:                            
+            break                
+        if is_par:                        
             i -= 1
-        elif estrelas[i] % 2 != 0:
-            fazendas_visitadas[i] = 1
-            estrelas[i] -= 1
-            i += 1
-
+        else:            
+            i += 1    
     print(f'{sum(fazendas_visitadas)} {sum(estrelas)}')
