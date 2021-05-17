4d287cf296ce1fccb75491bf0a5270664a89c441
dyogobendo <dyogoromagnabendo@gmail.com>
2021-05-17 13:16:30 -0300

    Gangorra

---
+++ ad_hoc/2455.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    p1, c1, p2, c2 = map(int, input().split())
+    r1 = p1 * c1
+    r2 = p2 * c2
+
+    if r1 > r2:
+        print(-1)
+    if r1 < r2:
+        print(1)
+    else:
+        print(0)
+    
