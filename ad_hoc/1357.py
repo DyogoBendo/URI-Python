d4657fad6b5603f54b1d5dac9c970db453816e51
dyogobendo <dyogoromagnabendo@gmail.com>
2021-06-11 22:19:55 -0300

    Problemas de imprimir

--- ad_hoc/1357.py
+++ ad_hoc/1357.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
-    numbers = [".*\n**\n.." ,"*.\n..\n..", "*.\n*.\n..", "**\n..\n..", "**\n.*\n..", "*.\n.*\n..", "**\n*.\n..", "**\n**\n..", "*.\n**\n..", ".*\n*.\n.."]
+    numbers = [ [".*", "*.", "*.", "**", "**", "*.", "**", "**", "*.", ".*"], ["**", "..", "*.", "..", ".*", ".*", "*.", "**", "**", "*."], ["..", "..", "..", "..", "..", "..", "..", "..", "..", ".."]]
+    numbers2 = [".* \n** \n.." ,"*. \n.. \n..", "*. \n*. \n..", "** \n.. \n..", "** \n.* \n..", "*. \n. *\n..", "** \n*. \n..", "** \n** \n..", "*. \n** \n..", ".* \n*. \n.."]
+
 
     while True:
         n = int(input())
@@ -8,13 +10,15 @@ if __name__ == '__main__':
         d = input()
         if d == "S":
             v = input()
-            for c in v:
-                print(numbers[int(c)], end='')                        
+            for i in range(3):
+                for c in v:
+                    print(numbers[i][int(c)], end="")
+                print()
         else:
             v = [""] * n
             for i in range(3):
-                v[i] += input() + '\n'
+                v[i] += input() + "\n"
             for c in v:
-                print(numbers.index(c), end='')
+                print(numbers2.index(c), end='')
 
 
